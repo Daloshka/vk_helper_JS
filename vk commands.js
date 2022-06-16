9fa9194b2be55909533fbbbafe3b05171045e71a
Lerrrkk <36123350+Daloshka@users.noreply.github.com>
2022-06-16 19:04:44 +0800

    Update vk commands.js

--- vk commands.js
+++ vk commands.js
@@ -18,3 +18,13 @@ b = document.querySelectorAll('.FlatButton')
 for (let i = 4; i < document.querySelectorAll('.FlatButton').length; i++) { // выведет 0, затем 1, затем 2
   b[i].click()
 }
+
+
+# ДОСТАЁТ ID УЧАСТНИКОВ ГРУППЫ
+
+b = document.querySelectorAll("#results > div > div.info > div.labeled.name > a")
+ids = []
+for (let i = 4; i < document.querySelectorAll('.FlatButton').length; i++) { // выведет 0, затем 1, затем 2
+  id = b[i].href.split('/')[3]
+  result.push(id)
+}
